d37769d3b4725bb008cdcb4d4c0993d50c2f3b8b
antonibote <antoni.bote@estudiantat.upc.edu>
2023-09-16 13:31:06 +0200

    Types, Arrays and Tuples

---
+++ salida.js
@@ -0,0 +1,18 @@
+console.log("Hello World");
+//types 
+var myString = "Hello World";
+myString = 22 + "";
+var myNumber = 4;
+var myBool = false;
+var myVar = "hello";
+myVar = false;
+//arrays
+var StringArray = ["item1", "item2", ""];
+var NumberArray = [1, 2, 3, 4];
+var BooleanArray = [true, false, false];
+var AnyArray = [1, 2, true, "hello", []];
+//tuple
+var StrNumTuple;
+StrNumTuple = ["hello", 22];
+//void, undefined, null
+document.write(myNumber.toString());
---
+++ salida.ts
@@ -0,0 +1,28 @@
+console.log("Hello World");
+
+//types 
+
+var myString : string = "Hello World";
+myString = 22 + "";
+var myNumber : number = 4;
+var myBool : boolean = false;
+var myVar : any = "hello";
+myVar = false;
+
+//arrays
+
+var StringArray : string[] = ["item1","item2",""];
+var NumberArray : number[] = [1, 2, 3, 4];
+var BooleanArray : boolean[] = [true, false, false];
+var AnyArray : any[] = [1, 2, true, "hello", []];
+
+//tuple
+
+var StrNumTuple : [string, number];
+StrNumTuple = ["hello", 22];
+
+//void, undefined, null
+
+
+
+document.write(myNumber.toString());
